abdf5b285c8293df0a1409becb2374caceaa9ff7
MangJo <mangune.jomarc@gmail.com>
2017-01-20 15:11:11 +0800

    Some re-factoring, added comments on some functions and I think its a stable release.

--- pso2_logviewer/FormMain.cs
+++ pso2_logviewer/FormMain.cs
@@ -15,68 +15,37 @@ namespace pso2_logviewer
 {
     public partial class FormMain : Form
     {
+        //All of the retrieved data are stored in a DataTable object.
         DataTable dt_txtfiles = new DataTable();
+        DataTable dt_txtcontents = new DataTable();
+
+        String[] columnNames = new String[] {"DateTime", "ChatLine", "ChatType", "PlayerID", "CharName", "Chat"};
 
         public FormMain()
         {
             InitializeComponent();
-            
-            refreshContents();
         }
 
+        //Refresh contents particularly on Text files list.
         private void refreshContents()
         {
-            if(Settings.loadSettings())
+            if (Settings.loadSettings())
             {
                 labelCurrentFolder.Text = Settings.logPathDir;
                 //loadTxtFilesToDGV_List(dataGridView1, Settings.logPathDir, "ChatLog*");
-                
-                dt_txtfiles = loadTxtFiles_as_dataTable(Settings.logPathDir, "ChatLog*");
-                dataGridView1.DataSource = dt_txtfiles;
 
-                statusStrip1.Items[0].Text = "Found " + dataGridView1.RowCount + " text files.";
+                dt_txtfiles = Mastermind.loadTxtFiles_as_dataTable(Settings.logPathDir, "ChatLog*");
+                dgv_textList.DataSource = dt_txtfiles;
+
+                statusStrip1.Items[0].Text = "Found " + dgv_textList.RowCount + " text file(s).";
             }
             else
             {
-               if( Settings.saveSettings(Settings.selectFolder()))
+                if (Settings.saveSettings(Settings.selectFolder()))
                 {
                     refreshContents();
                 }
             }
-
-        }
-
-        private void loadTxtFilesToDGV_List(DataGridView data_grid_view, string folder, string filename)
-        {
-            if (data_grid_view.RowCount >= 0)
-            {
-                data_grid_view.Rows.Clear();
-                DirectoryInfo dirInfo = new DirectoryInfo(folder);
-                FileInfo[] files = dirInfo.GetFiles(filename);
-                foreach (FileInfo file in files)
-                {
-                    data_grid_view.Rows.Add(file.Name);
-                }
-            }
-        }
-
-        //EXPERIMENTAL: Load Text files list into a DataTable
-        private DataTable loadTxtFiles_as_dataTable(string folder, string filename)
-        {
-            DataTable dt = new DataTable("txtFilesList");
-
-            dt.Columns.Add("Logs");
-
-            DirectoryInfo dirInfo = new DirectoryInfo(folder);
-            FileInfo[] files = dirInfo.GetFiles(filename);
-            foreach (FileInfo file in files)
-            {
-                DataRow dr = dt.NewRow();
-                dr[0] = file.Name;
-                dt.Rows.Add(dr);
-            }
-
-            return dt;
         }
 
         private void btnChangeFolder_Click(object sender, EventArgs e)
@@ -87,118 +56,91 @@ namespace pso2_logviewer
             }
         }
 
-        private void loadChatTextContents()
+        private DataTable loadChatTextContents()
         {
-            //Experimental Implementation
-            string temp = Settings.logPathDir + "\\" + dataGridView1.CurrentCell.Value;
+            //Store logPathDir value in a string variable.
+            string text_filename = Settings.logPathDir + "\\" + dgv_textList.CurrentCell.Value;
 
-            if (File.Exists(temp) == false)
+            //Sanity-check: If the file does not exist, even if it is on the file list.
+            if (File.Exists(text_filename) == false)
             {
-                MessageBox.Show("Ooops, it seems this log file doesn't exist, another app might removed it from the directory","No file found", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
-                return;
+                MessageBox.Show("Ooops, it seems this log file doesn't exist, another app might removed it from the directory", "No file found", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return null;
             }
 
-            //Create column names
-            String[] columnNames = new String[]
-            {
-                "DateTime", "ChatLine", "ChatType", "PlayerID", "CharName", "Chat"
-            };
-
-            //Create DataTable
+            //Create a local DataTable
             DataTable dt = new DataTable();
 
-            //Add columns and apply column names. Code Refactor as addColumnNamesToDataTable(DataTable data_table, String[] column_names)
-            foreach (string column_name in columnNames)
-            {
-                dt.Columns.Add(column_name);
-            }
+            //Add columns and apply column names.
+            Mastermind.addColumnNamesToDataTable(dt, columnNames);
 
-            //After adding the columns, next is to load the text file
-            //and insert it's contents into DataTable
-
-            //Open the text file; read file line by line; detect the delimiter in this case the TAB
-            //Use a Data Row to hold all the data read from each line.
-            //After insertion of data into Data Row, append the Data Row into the DataTable
+            //Open the text file; read text line by line; detect the delimiter, in this case the TAB \t
+            //Use a DataRow to hold all the data read from the line.
+            //After insertion of data into the DataRow, append the DataRow into the DataTable
             //Repeat until the End Of Stream
-            //Display DataTable on DataGridView Control
-            using (StreamReader sr = new StreamReader(temp))
+            using (StreamReader sr = new StreamReader(text_filename))
             {
+                StringBuilder strBuilder = new StringBuilder(); //Holds the text from a linebroke chat (Some chats contain \r\n as in breaking down continuous text with a linebreak)
+                string temporary_text = ""; //Holds a copy of text of the Chat's main row and is to be appended with other text from linebreaks.
+                int rowIndex = -1; //Tracks the current row in process.
+                int noOfNextLines = 0; //Tracks the number of linebreaks from the chat's main row.
+
                 //Opens the file until the end of stream
                 while (!sr.EndOfStream)
                 {
                     var line = sr.ReadLine();
                     var values = line.Split('\t');
 
-                    DataRow dr = dt.NewRow();
+                    //If the values.Length is 1, this means it is a linebroke chat.
+                    if (values.Length == 1)
+                    {
+                        strBuilder.Append(" " + values[0]);
+                        noOfNextLines++;
+                        continue;
+                    }
+                    else if (noOfNextLines == 2) //The MAXIMUM Linebreak from a multi-line chat is 2. If the count is 2, then append texts into a single line.
+                    {
+                        dt.Rows[rowIndex].SetField(5, temporary_text + strBuilder.ToString()); //rowIndex hasn't been incremented because it detected a multi-line chat. Therefore, it is still the current row in process.
+                        strBuilder.Clear(); //After appending text, clear the contents of the stringbuilder object.
+                        noOfNextLines = 0; //Reset to zero.
+                       // continue;
+                    }
 
-                    //Put the values on the respective column
+                    DataRow dr = dt.NewRow();
+                    rowIndex++;
+                    temporary_text = values[5];
+
+                    //Put the values on the respective columns.
                     for (int i = 0; i < dt.Columns.Count; i++)
                     {
                         if (values.Length > 1)
                             dr[i] = values[i];
-                        //This one is a tricky, there are text lines that uses newline as in continuation
-                        //of previous chat on a new line (hard to explain in words lol)
-                        //something like this:
-                        //Chat: I'm selling this
+                        //This one is tricky, there are text of chat that uses breakline as in continuation
+                        //of that chat on a new line.
+
+                        //Something like this:
+                        //Chat: I'm selling this one
                         //      Item for
-                        //      500M
+                        //      500M only.
 
                         //Instead of being 1 line:
-                        //Chat: I'm selling this Item for 500M
-                        //[5] is the index number for chat column, [0] is the newline text
-                        //it is originally for datetime column
-                        else
-                        {
-                            dr[5] = values[0];
-                        }
+                        //Chat: I'm selling this one Item for 500M only.
+                        //This problem has been fixed or a work around has been implemented, please see the above comments.
                     }
                     dt.Rows.Add(dr);
                 }
-                dataGridView2.DataSource = dt;
+
+                return dt;
+                //dataGridView2.DataSource = dt;
                 //dataGridView2.Refresh();
+                //MessageBox.Show(dataGridView2.RowCount.ToString());
             }
         }
-
-        private void colorizeRow_ChatLine(DataGridView dgv, int colIndex, Dictionary<string, Color> chat_type_color)
+        
+        private void applyTextColorOnDGV_contents()
         {
-            Color colorizer_temp = new Color();
-            //Selects a row
-            for (int ctr = 0; ctr < dgv.Rows.Count; ctr++)
-            {
-                //Get the chat type from the dgv
-                var str_msg = dgv[colIndex, ctr].Value.ToString();
-
-                
-                //Loop through the dictionary that holds key value pair CHAT_TYPE:COLOR in this case.
-                foreach (var chat_type in chat_type_color)
-                {
-                    //If it found a match, place the color on a var then break out of loop.
-                    if (str_msg == chat_type.Key)
-                    {
-                        colorizer_temp = chat_type_color[chat_type.Key];
-                        break;
-                    }
-                    //this is for the continuation of in-game chat multi-line (I'd say its bad implementation)
-                    if (str_msg == "")
-                    {
-                        break;
-                    }
-                    //if it did not found any matching key, continue to the next key until it finds one
-                    else
-                    {
-                        continue;
-                    }
-                }
-
-                //Colorize the text on the selected row
-                dgv.Rows[ctr].DefaultCellStyle.ForeColor = colorizer_temp;
-            }
-        }
-
-        private void applyTextColorOnDGV()
-        {
-            //Coloring the text of the row according to its chat type
-            dataGridView2.DefaultCellStyle.BackColor = Color.Black;
+            //Coloring the text of the row according to its chat type.
+            dgv_logContents.DefaultCellStyle.BackColor = Color.Black;
 
             var chatTypeColor = new Dictionary<string, Color>();
             chatTypeColor.Add("PUBLIC", Color.White);
@@ -206,92 +148,93 @@ namespace pso2_logviewer
             chatTypeColor.Add("GUILD", Color.Orange);
             chatTypeColor.Add("REPLY", Color.Violet);
 
-            colorizeRow_ChatLine(dataGridView2, 2, chatTypeColor);
+            Mastermind.colorizeRow_ChatLine(dgv_logContents, 2, chatTypeColor);
         }
 
-        //you may want to add code for storing and loading checked values on an XML file
-        private void HideUnhide_DGVColumns_by_checkBox(CheckBox check_box, DataGridView dgv, int column_index)
-        {
-            if(dgv.ColumnCount == 0)
-            {
-                return;
-            }
-
-            if (check_box.Checked == true)
-            {
-                dgv.Columns[column_index].Visible = true;
-            } else
-            {
-                dgv.Columns[column_index].Visible = false;
-            }
-        }
-
+        //dataGridView1 is dgv_textList
         private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
         {
-            loadChatTextContents();
+            dt_txtcontents = loadChatTextContents();
+            dgv_logContents.DataSource = dt_txtcontents;
         }
 
-        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
-        {
-           // loadChatTextContents();
-        }
-
+        //dataGridView2 is dgv_logContents
         private void dataGridView2_DataBindingComplete(object sender, DataGridViewBindingCompleteEventArgs e)
         {
-            applyTextColorOnDGV();
-        }
-
-        //EXPERIMENTAL
-        private void doSearch(DataTable data_table, DataGridView dgv, string column_name, string search_text)
-        {
-            if (data_table != null)
-            {
-                DataView local_dv = new DataView(data_table);
-                local_dv.RowFilter = column_name + " like '%" + search_text + "%'";
-                dgv.DataSource = local_dv;
-            }
-            else
-            {
-                MessageBox.Show("No results found.", "Search", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            }
+            applyTextColorOnDGV_contents();
         }
 
         #region CHECKBOXES
+
+        //checkBox1 is cb_Date
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
-            HideUnhide_DGVColumns_by_checkBox(checkBox1, dataGridView2, 0);
+            Mastermind.HideUnhide_DGVColumns_by_checkBox(cb_Date, dgv_logContents, 0);
         }
 
+        //checkBox2 is cb_lineNo
         private void checkBox2_CheckedChanged(object sender, EventArgs e)
         {
-            HideUnhide_DGVColumns_by_checkBox(checkBox2, dataGridView2, 1);
+            Mastermind.HideUnhide_DGVColumns_by_checkBox(cb_lineNo, dgv_logContents, 1);
         }
 
+        //checkBox3 is cb_chatType
         private void checkBox3_CheckedChanged(object sender, EventArgs e)
         {
-            HideUnhide_DGVColumns_by_checkBox(checkBox3, dataGridView2, 2);
+            Mastermind.HideUnhide_DGVColumns_by_checkBox(cb_chatType, dgv_logContents, 2);
         }
 
+        //checkBox4 is cb_playerID
         private void checkBox4_CheckedChanged(object sender, EventArgs e)
         {
-            HideUnhide_DGVColumns_by_checkBox(checkBox4, dataGridView2, 3);
+            Mastermind.HideUnhide_DGVColumns_by_checkBox(cb_playerID, dgv_logContents, 3);
         }
 
+        //checkBox5 is cb_PCName
         private void checkBox5_CheckedChanged(object sender, EventArgs e)
         {
-            HideUnhide_DGVColumns_by_checkBox(checkBox5, dataGridView2, 4);
+            Mastermind.HideUnhide_DGVColumns_by_checkBox(cb_PCName, dgv_logContents, 4);
         }
 
-        private void checkBox6_CheckedChanged(object sender, EventArgs e)
+        //checkBox6 is cb_Chat
+        private void cb_Chat_CheckedChanged(object sender, EventArgs e)
         {
-            HideUnhide_DGVColumns_by_checkBox(checkBox6, dataGridView2, 5);
+            Mastermind.HideUnhide_DGVColumns_by_checkBox(cb_Chat, dgv_logContents, 5);
         }
         #endregion
 
+        //search|filter on text files list.
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            doSearch(dt_txtfiles, dataGridView1, "Logs", textBox1.Text);
-            statusStrip1.Items[0].Text = "Found " + dataGridView1.RowCount + " text files.";
+            Mastermind.doSearch(dt_txtfiles, dgv_textList, "Logs", txtBox_TxtFileListFilter.Text);
+            statusStrip1.Items[0].Text = "Found " + dgv_textList.RowCount + " text file(s).";
+        }
+
+        //search|filter on log contents.
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            search_logContents();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            Mastermind.addItemsToComboBoxFilter(comBox_ColNames, columnNames);
+            refreshContents();
+        }
+
+        private void textBox2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if ( e.KeyCode == Keys.Enter)
+            {
+                search_logContents();
+            }
+        }
+
+        //Used by some events to trigger this function
+        private void search_logContents()
+        {
+            if (comBox_ColNames.Text == "") { return; }
+            Mastermind.doSearch(dt_txtcontents, dgv_logContents, comBox_ColNames.Text, txtBox_ContentsFilter.Text);
         }
     }
 }
